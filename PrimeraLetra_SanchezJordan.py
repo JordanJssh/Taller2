5913ff10cb4cf44843732db081d40e0c7c5f15c9
USUARIO13-PC\Usuario01 <jordan_jssh_@hotmail.com>
2016-12-21 12:52:11 -0500

    Ejercicio primera letra (Jordan Sanchez)

---
+++ PrimeraLetra_SanchezJordan.py
@@ -0,0 +1,26 @@
+import turtle
+
+t=turtle.Pen()
+
+t.forward(150)
+t.right(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+t.left(90)
+t.forward(150)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(40)
+t.right(90)
+t.forward(50)
+t.left(90)
+t.forward(110)
+t.left(90)
+t.forward(50)
+t.right(90)
+t.forward(50)
+
+
+
